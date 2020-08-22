15f562a3d18cad4d433d534e3b7dc0c1e017e7a3
Yonar <yonar1687@gmail.com>
2020-08-22 00:14:47 -0300

    primer commit, pendientes algunos bugs

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const routes = require('./routes');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+// Cors permite que un cliente se conecta a otro servidor para el intercambio de recursos
+
+const cors = require('cors');
+// Definir un dominio para recibir las peticiones
+const whitelist = ['http://localhost:3000'];
+
+// conectar mongo
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/restapis', {
+    useNewUrlParser: true
+});
+
+// crear el servidor
+const app = express();
+
+// habilitar bodyparser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+// Habilitar cors
+app.use(cors());
+
+// Rutas de la app
+app.use('/', routes());
+
+// carpeta publica
+app.use(express.static('uploads'));
+
+// puerto
+app.listen(5000, ()=>{
+    console.log('Server Linstening')
+});
