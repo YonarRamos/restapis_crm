a278a71dcbf4ad34f5c009c64ef6f44a88b405ee
Yonar <yonar1687@gmail.com>
2020-09-09 14:11:05 -0300

    prepararando para deployment

--- index.js
+++ index.js
@@ -44,9 +44,9 @@ app.use(cors(corsOptions));
 // Rutas de la app
 app.use('/', routes());
 
-const host = process.env.HOST || '0.0.0.0';
+//const host = process.env.HOST || '0.0.0.0';
 const port = process.env.PORT || 5000;
 // puerto
-app.listen(port, host,  ()=>{
+app.listen(port,  ()=>{
     console.log('Server Linstening')
 });
